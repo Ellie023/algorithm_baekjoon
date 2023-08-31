6b456c75d781ab7275553459c4e1aa8fa6f33dde
yelin56 <100378349+yelin56@users.noreply.github.com>
2023-09-01 04:44:00 +0900

    complete

--- 25206.cpp
+++ 25206.cpp
@@ -2,7 +2,9 @@
 #include <map>
 #include "string"
 using namespace std;
-
+struct node{
+    float value=0;
+};
 int main() {
 
     map<string,float> grade;
@@ -10,17 +12,21 @@ int main() {
     int n=20;
     float a;
     string b;
-    while (n--)
+    float sum=0;//학점의 총합
+    while (n--)//20줄
     {
-        float sum=0;//학점의 총합
-        cin>>s>>a>>b;
-        if(!grade[b])
-            grade[b]=1;
-        else
-            grade[b]++;
-        if(b!="P");
 
+        cin>>s>>a>>b;//입력
+        grade[b]+=a; //전공과목별로 학점 저장 ex) grade[A+]=3.0+4.0+a
+        if(b!="P")//P는 계산하지 않는다.
+            sum+=a;
     }
+    std::cout.precision(7);
+    float g;
+    g=(grade["A+"]*4.5)+(grade["A0"]*4.0)+(grade["B+"]*3.5)+(grade["B0"]*3.0)+(grade["C+"]*2.5
+    )+(grade["C0"]*2.0)+(grade["D+"]*1.5)+(grade["D0"]*1.0)+(grade["F"]*0.0);
 
+    g/=sum;//전공평점은 전공과목별 (학점 × 과목평점)의 합을 학점의 총합으로 나눈 값
+    cout<<g;//출력
     return 0;
 }

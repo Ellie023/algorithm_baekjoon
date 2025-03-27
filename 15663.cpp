8f2a7406b5d6d15c1d0dff0308e5c160c1e7dd68
Ellie023 <sylin0820@ewhain.net>
2025-03-27 14:57:49 +0900

    3/27 update

--- 15663.cpp
+++ 15663.cpp
@@ -1,25 +1,54 @@
 #include <iostream>
-#include "vector"
-#include "set"
-#include "queue"
+#include <vector>
+#include <set>
+#include <queue>
+#include <algorithm>
+
 using namespace std;
+
+#define MAX 9
+
+int N,M;
+int ans[MAX];
+int arr[MAX];
+bool visited[MAX];
+set<vector<int>> s;
+
+void dfs(int k){
+    if(k==M){
+        vector<int> v;
+        for(auto i=0; i<M; i++){
+            v.push_back(ans[i]);
+        }
+        s.insert(v);
+        v.clear();
+    }
+    else{
+        for(auto i=0; i<N; i++){
+            if(!visited[i]){
+                visited[i]=true;
+                ans[k]=arr[i];
+                dfs(k+1);
+                visited[i]=false;
+            }
+        }
+    }
+}
 
 int main() {
 
-    int n,m;
-    cin>>n>>m;
-    priority_queue<int> v;
-    vector<int> a;
-    for(int i=0; i<n; i++){
-        int b;
-        cin>>b;
-        v.push(b);
-        a.push_back(b);
+    cin>>N>>M;
+
+    for(int i=0; i<N; i++){
+        cin>>arr[i];
     }
-
-    for(int i=0; i<m; i++){
-        cout<<v.top();
-
+    sort(arr,arr+N);
+    dfs(0);
+    for(auto vector:s){
+        for(auto temp:vector){
+            cout<<temp<<" ";
+        }
+        cout<<'\n';
     }
     return 0;
 

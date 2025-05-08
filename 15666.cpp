19698c831b8dc23475e883c7867d0c9b2efc2df5
Ellie023 <sylin0820@ewhain.net>
2025-05-08 23:32:56 +0900

    05/08 update

--- 15666.cpp
+++ 15666.cpp
@@ -1,39 +1,40 @@
 #include <iostream>
-#include<algorithm>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
-int n,m;
-vector<int> ans,check;
-void bracking(int cnt){
-    if(cnt==m){
-        for(int i=0; i<check.size(); i++){
-            cout<<check[i]<<" ";
-        }
-        cout<<'\n';
+
+int n, m;
+vector<int> nums;
+int arr[9];
+
+void dfs(int start, int depth) {
+    if (depth == m) {
+        for (int i = 0; i < m; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
         return;
     }
-    int num=-2;
-    for(int i=0; i<n; i++){
-        if(ans[i]==num){
-            continue;
-        }
-        num=ans[i];
-        check.push_back(num);
-        bracking(cnt+1);
 
-        check.pop_back();//반납
-
+    for (int i = start; i < n; i++) {
+        arr[depth] = nums[i];
+        dfs(i, depth + 1);
     }
-    return;
 }
+
 int main() {
-    //입력
-    cin>>n>>m;
-    for(int input,i=0; i<n; i++){
-        cin>>input;
-        ans.push_back(input);
-    }
-    sort(ans.begin(),ans.end());
-    bracking(0);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    nums.resize(n);
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+
+    sort(nums.begin(), nums.end());  // 정렬
+    nums.erase(unique(nums.begin(), nums.end()), nums.end());  // 중복 제거
+    n = nums.size(); // 중복 제거 후 길이로 업데이트
+
+    dfs(0, 0);
     return 0;
 }

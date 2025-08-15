d0a680598470db26764ba71f74276d4077c7b46b
Ellie023 <sylin0820@ewhain.net>
2025-08-15 14:56:01 +0900

    8/15 update

--- 2467.cpp
+++ 2467.cpp
@@ -3,27 +3,30 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    int N;
-    if (!(cin >> N)) return 0;
-    vector<long long> a(N);
-    for (int i = 0; i < N; ++i) cin >> a[i];
+    int n;
+    cin>>n;
+    vector<int> a(n);
 
-    int L = 0, R = N - 1;
-    long long bestSum = LLONG_MAX;
-    long long ansL = a[L], ansR = a[R];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    int L=0;
+    int R=n-1;
+    long long bestsum=LLONG_MAX;
+    long long ansL=a[L], ansR=a[R];
 
-    while (L < R) {
-        long long s = a[L] + a[R];
-        if (llabs(s) < llabs(bestSum)) {
-            bestSum = s;
-            ansL = a[L];
-            ansR = a[R];
-            if (bestSum == 0) break; // 더 볼 필요 없음
+    while(L<R){
+        long long s=a[L]+a[R];
+        if(llabs(s)<llabs(bestsum)){
+            bestsum=s;
+            ansL=a[L],ansR=a[R];
+            if(bestsum==0) break;
         }
-        if (s < 0) ++L;     // 합을 키우기 위해 왼쪽을 오른쪽으로
-        else --R;           // 합을 줄이기 위해 오른쪽을 왼쪽으로
+        if(s<0) L++;
+        else R--;
     }
 
     cout << ansL << ' ' << ansR << '\n';
